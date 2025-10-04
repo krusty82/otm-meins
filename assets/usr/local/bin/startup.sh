9689c88a8740e8caead724456ed195a9219a5579
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-10-04 13:43:45 +0200

    Update startup.sh

--- assets/usr/local/bin/startup.sh
+++ assets/usr/local/bin/startup.sh
@@ -17,22 +17,34 @@ if [ ! -d "/mnt/tiles" ]; then
 fi
 
 /etc/init.d/postgresql start
-/etc/init.d/rsyslog start
-/etc/init.d/tirex-backend-manager start
-/etc/init.d/tirex-master start
-/etc/init.d/ssh start
+#/etc/init.d/rsyslog start
+#sudo mkdir /var/run/tirex
+#sudo chown -R tirex:tirex /var/run/tirex
+sudo -u tirex tirex-backend-manager start
+sudo -u tirex tirex-master start
+#/etc/init.d/ssh start
+
+
+
+# Starte rsyslog (optional, für Logging)
+#rsyslogd
+
+
+
+
 
 sed -i 's/$SERVER_NAME/'"$DOMAIN"'/g' /etc/apache2/sites-available/tileserver_site.conf
 sed -i 's/$WHITELIST/'"$WHITELIST"'/g' /etc/apache2/sites-available/tileserver_site.conf
 sed -i 's/$DOMAIN/'"$DOMAIN"'/g' /var/www/mapdemo/index.html
+a2ensite tileserver_site.conf
 
 if [ "$LETSENCRYPT" = "1" ]; then
   certbot -n --apache --cert-name mapserver --redirect --agree-tos --email "$EMAIL" --domain "$DOMAIN"
 fi
 
-if [ "$MOD_TILE_PREVENT_EXPIRATION" = "1" ]; then
-  touch -d '10 years ago' /var/lib/mod_tile/planet-import-complete
-fi
+#if [ "$MOD_TILE_PREVENT_EXPIRATION" = "1" ]; then
+#  touch -d '10 years ago' /var/lib/mod_tile/planet-import-complete
+#fi
 
 apachectl stop
 sleep 10
