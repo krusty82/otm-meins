ee834647eca554ac2ad845115c577ae41243916e
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-10-04 11:08:11 +0200

    Update 06_dem_contours2.sh

--- assets/scripts/06_dem_contours2.sh
+++ assets/scripts/06_dem_contours2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd /mnt/data/srtm
-
+# Je nach Umfang der Höhendaten wird viel Speicher benötigt, ggfs swap size erhöhen. 
+# Für Europa reichen 16Gb RAM und etwa 20Gb Swap
 # import contours data into db
 createdb contours -O tirex
 psql -d contours -c 'CREATE EXTENSION postgis;'
