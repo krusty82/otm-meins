1b58b581f9c3971665524746ca4b61a766866298
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-10-15 08:49:16 +0200

    bei Neuimport db droppen und tablespace löschen

--- assets/scripts/00_setup_database.sh
+++ assets/scripts/00_setup_database.sh
@@ -2,11 +2,12 @@
 
 sudo -u postgres psql -c 'CREATE USER root';
 sudo -u postgres psql -c 'ALTER USER root WITH SUPERUSER';
+dropdb gis
 createuser tirex
 createdb gis
 psql -d gis -c 'CREATE EXTENSION postgis;'
 psql -d gis -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO tirex;'
 psql -d gis -c 'GRANT CONNECT ON DATABASE gis TO tirex;'
-
+psql -d gis -c 'DROP TABLESPACE hdd;'
 #Funktion zur Namensanpassung hinzufügen
 psql -f get_localized_name.sql gis
