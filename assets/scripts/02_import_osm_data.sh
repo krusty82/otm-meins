7915f0d0f2887ec589f87fa67d484ccea32f9ad8
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-10-17 10:32:12 +0200

    Update 02_import_osm_data.sh

--- assets/scripts/02_import_osm_data.sh
+++ assets/scripts/02_import_osm_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MEMORY=20000  # change this to the memory you have available
+MEMORY=16000  # change this to the memory you have available
 # Suche nach .pbf-Datei im Ordner /mnt/data
 DATA_FILE=$(find /mnt/data -maxdepth 1 -type f -name "*.pbf" | head -n 1)
 
@@ -16,7 +16,7 @@ chown postgres /mnt/db/*
 sudo -u postgres psql -d gis -c "CREATE TABLESPACE hdd LOCATION '/mnt/db/tiles';" && \
 
 sudo -u postgres osm2pgsql -U postgres --slim --drop -d gis -C $MEMORY --tablespace-slim-data hdd --tablespace-slim-index hdd \
-          --number-processes 10 --flat-nodes /mnt/db/flat-nodes/gis-flat-nodes.bin  \
+          --number-processes 4 --flat-nodes /mnt/db/flat-nodes/gis-flat-nodes.bin  \
           --style /home/otm/db/opentopomap.style $DATA_FILE
 
 echo "Re-Setting permissions for tirex"
