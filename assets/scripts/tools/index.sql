b130b442c58e9b580208a56d74f78330143aa108
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-10-18 11:19:23 +0200

    Update index.sql

--- assets/scripts/tools/index.sql
+++ assets/scripts/tools/index.sql
@@ -9,10 +9,28 @@ ON planet_osm_point(name);
 CREATE INDEX IF NOT EXISTS idx_point_natural
 ON planet_osm_point("natural");
 
--- Optional: evtl. Duplikat
-CREATE INDEX IF NOT EXISTS idx_point_osm_id
-ON planet_osm_point(osm_id);
+CREATE INDEX IF NOT EXISTS idx_planet_osm_polygon_osm_id
+ON planet_osm_polygon(osm_id);
 
--- GiST für Geometrie
-CREATE INDEX IF NOT EXISTS planet_osm_point_way_idx
-ON planet_osm_point USING GIST(way);
+-- Name
+CREATE INDEX IF NOT EXISTS idx_polygon_name
+ON planet_osm_polygon(name);
+
+-- Natural
+CREATE INDEX IF NOT EXISTS idx_polygon_natural
+ON planet_osm_polygon("natural");
+
+CREATE INDEX IF NOT EXISTS idx_planet_osm_line_osm_id
+ON planet_osm_line(osm_id);
+
+-- Name
+CREATE INDEX IF NOT EXISTS idx_line_name
+ON planet_osm_line(name);
+
+-- Natural
+CREATE INDEX IF NOT EXISTS idx_line_natural
+ON planet_osm_line("natural");
+
+
+
+
