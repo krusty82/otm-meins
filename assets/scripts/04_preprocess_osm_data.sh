a81c26100073a1e201483cd7317f3eb820c57b96
krusty82 <125685767+krusty82@users.noreply.github.com>
2025-12-24 01:23:14 +0100

    Add views.sql to preprocessing script

--- assets/scripts/04_preprocess_osm_data.sh
+++ assets/scripts/04_preprocess_osm_data.sh
@@ -4,6 +4,7 @@ cd tools
 cc -o saddledirection saddledirection.c -lm -lgdal
 cc -Wall -o isolation isolation.c -lgdal -lm -O2
 psql gis < index.sql
+psql gis < views.sql
 psql gis < arealabel.sql
 
 ./update_lowzoom.sh
