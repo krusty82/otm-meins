60b28db415597991b38d2515d58536a642b861b7
krusty82 <125685767+krusty82@users.noreply.github.com>
2026-03-02 22:29:17 +0100

    Refactor get_localized_placename function
    Refactor get_localized_placename function to always return name_en and remove unused variable.

--- assets/scripts/tools/get_localized_name.sql
+++ assets/scripts/tools/get_localized_name.sql
@@ -132,12 +132,8 @@ $$ LANGUAGE 'plpgsql';
 
 
 CREATE or REPLACE FUNCTION get_localized_placename(name text, local_name text, int_name text, name_en text, loc_in_brackets boolean) RETURNS TEXT AS $$
- DECLARE
-	tempname text;
  BEGIN
-	 tempname=local_name;  /*name:de as fallback*/
  	 local_name=name_en;  /*always return name_en*/ 
-	 name_en=tempname; 
     IF (local_name is NULL) THEN
       IF (int_name is NULL) THEN
     	IF (name_en is NULL) THEN
