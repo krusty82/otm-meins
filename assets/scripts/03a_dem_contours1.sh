0c45018a731cc1dbe9e7cffd436c7d390b8d67bf
krusty82 <125685767+krusty82@users.noreply.github.com>
2026-04-20 10:54:12 +0200

    Clarify script purpose and RAM requirements
    Updated comment to clarify the purpose of the script and added a note about RAM requirements.

--- assets/scripts/03a_dem_contours1.sh
+++ assets/scripts/03a_dem_contours1.sh
@@ -7,6 +7,6 @@ cd srtm
 
 # create contour lines - this takes a long time
 # and you need A LOT of RAM, at least 16 GB are needed!
-# phyghtmap -o contour --max-nodes-per-tile=0 -s 10 -0 --pbf warp-60.tif
+# reads a polyfile in the /mn/data folder and downloads the srtm files
 # Use pyhgtmap
 pyhgtmap --polygon ../*.poly --step=10 --pbf --hgtdir=hgt  --source=view3 --simplifyContoursEpsilon=0.0001
